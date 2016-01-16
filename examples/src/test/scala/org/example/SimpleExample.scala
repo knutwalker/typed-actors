466bebcaf5e16b9572d4b10adc25eaa5e940f16a
Paul Horn <knutwalker@gmail.com>
2016-01-16 15:41:42 +0100

    Update copyright header

--- examples/src/test/scala/org/example/SimpleExample.scala
+++ examples/src/test/scala/org/example/SimpleExample.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Paul Horn
+ * Copyright 2015 – 2016 Paul Horn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
